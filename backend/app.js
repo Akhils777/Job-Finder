2b2f69e21cfc0e0a18cce7ffffdbb7a0526b1f17
Akhils777 <akhilpratapsingh777@gmail.com>
2024-09-07 02:52:05 +0530

    create automation, sendEmail.js

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ import fileUpload from "express-fileupload";
 import userRouter from "./routes/userRouter.js";
 import jobRouter from "./routes/jobRouter.js";
 import applicationRouter from "./routes/applicationRouter.js"
+import { newsLetterCron } from "./automation/newsLetterCron.js";
 
 const app = express();
 config({path: "./.env"})
@@ -33,7 +34,7 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
 
-
+ newsLetterCron();
 connection();
 app.use(errorMiddleware)
 
